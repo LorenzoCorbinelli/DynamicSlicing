caca362b45759ade0086638fde23a40d234e8a69
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-04-14 15:19:13 +0200

    Refactoring

--- app/src/main/java/corbinelli/lorenzo/dynamicslicing/XPosedModule.java
+++ app/src/main/java/corbinelli/lorenzo/dynamicslicing/XPosedModule.java
@@ -4,8 +4,10 @@ import android.os.Build;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -18,53 +20,61 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XPosedModule implements IXposedHookLoadPackage {
 
+    private JSONArray readJSON() throws IOException, JSONException {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("res/raw/hooks.json");
+        String json = "";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            json = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        }
+        return new JSONArray(json);
+    }
+
+    private void addCallback(Object[] parametersAndHook) {
+        parametersAndHook[parametersAndHook.length - 1] = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Log.d("LSPosedDebug", "Args: " + Arrays.toString(param.args));
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Log.d("LSPosedDebug", "Result: " + param.getResult());
+            }
+        };
+    }
+
+    private Object[] getParametersTypeAndCallback(JSONObject hook) throws JSONException {
+        JSONArray jsonParameters = hook.getJSONArray("parametersType");
+        Object[] parametersAndHook = new Object[jsonParameters.length() + 1];   // as the last element will be the callback
+        for (int i = 0; i < jsonParameters.length(); i++) {
+            // take ech parameterType as string
+            String parameterType = jsonParameters.getString(i);
+            try {
+                parametersAndHook[i] = Class.forName(parameterType);
+            } catch (ClassNotFoundException e) {
+                // TODO: generalize this check with other types
+                if (parameterType.equals("byte[]")) {
+                    parametersAndHook[i] = byte[].class;
+                }
+                // in case there's not a reference of the class it will be specified as a string
+                parametersAndHook[i] = parameterType;
+            }
+        }
+        addCallback(parametersAndHook);
+        return parametersAndHook;
+    }
+
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         Log.d("LSPosedDebug", "Hooking into " + lpparam.packageName);
 
         try {
-            // read the json file
-            InputStream is = getClass().getClassLoader().getResourceAsStream("res/raw/hooks.json");
-            String json = "";
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                json = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-            }
-            JSONArray jsonArray = new JSONArray(json);
+            JSONArray jsonArray = readJSON();
 
             for (int i = 0; i < jsonArray.length(); i++) {
                 // take each element in the json
                 JSONObject hook = jsonArray.getJSONObject(i);
-                // take the parametersType field
-                JSONArray jsonParameters = hook.getJSONArray("parametersType");
-
-                Object[] parametersAndHook = new Object[jsonParameters.length() + 1];
-                for (int j = 0; j < jsonParameters.length(); j++) {
-                    // take ech parameterType as string
-                    String parameterType = jsonParameters.getString(j);
-                    try {
-                        parametersAndHook[j] = Class.forName(parameterType);
-                    } catch (ClassNotFoundException e) {
-                        // TODO: generalize this check with other types
-                        if (parameterType.equals("byte[]")) {
-                            parametersAndHook[j] = byte[].class;
-                        }
-                        // in case there's not a reference of the class it will be specified as a string
-                        parametersAndHook[j] = parameterType;
-                    }
-                }
-
-                // add the callback as the last element
-                parametersAndHook[parametersAndHook.length - 1] = new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        Log.d("LSPosedDebug", "Args: " + Arrays.toString(param.args));
-                    }
-
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Log.d("LSPosedDebug", "Result: " + param.getResult());
-                    }
-                };
+                Object[] parametersAndHook = getParametersTypeAndCallback(hook);
 
                 if (hook.has("methodName")) {
                     XposedHelpers.findAndHookMethod(
