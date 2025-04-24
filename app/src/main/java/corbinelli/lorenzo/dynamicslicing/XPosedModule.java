91f882f2a8e8d29af629b1b4449c851ed03f7df6
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-04-24 17:02:11 +0200

    Removed the log file write

--- app/src/main/java/corbinelli/lorenzo/dynamicslicing/XPosedModule.java
+++ app/src/main/java/corbinelli/lorenzo/dynamicslicing/XPosedModule.java
@@ -1,15 +1,12 @@
 package corbinelli.lorenzo.dynamicslicing;
 
 import android.os.Build;
-import android.os.Environment;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -52,14 +49,15 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
   }
  */
 
-// TODO: rivedere per i tipi generici e probabilmente togliere lo switch
 // TODO: modificare i log in modo da avere una struttura più a codice del tipo: tipo di ritorno - metodo(args)
+//  il metodo after con il valore di ritorno non serve per lo slice ma può essere usato come commento
+//  per dire che deve ritornare quel valore. Attualmente se result è un array stampa il riferimento e non il contenuto
+//  cosa che nel before è stata risolta con Arrays.deepToString avendo un array e non un oggetto.
+
 // TODO: tool che salva i log in un file: prende solo i log utili togliendo il resto. Controllare Log
 //  che probabilmente ha un metodo che stampa meno informazioni rispetto a debug (d)
 
 public class XPosedModule implements IXposedHookLoadPackage {
-
-    private final String logFileName = "/Documents/xposed_log.txt";
 
     private JSONArray readJSON() throws IOException, JSONException {
         InputStream is = getClass().getClassLoader().getResourceAsStream("res/raw/hooks.json");
@@ -70,39 +68,17 @@ public class XPosedModule implements IXposedHookLoadPackage {
         return new JSONArray(json);
     }
 
-    private void writeLogToFile(String content) {
-        File logFile = new File(Environment.getExternalStorageDirectory().getPath() + logFileName);
-        try {
-            FileWriter writer = new FileWriter(logFile, true);
-            writer.append(content);
-            writer.append("\n");
-            writer.close();
-        } catch (IOException e) {
-            Log.e("LSPosedDebug", e.getMessage());
-        }
-    }
-
-    private void clearLogFile() {
-        File logFile = new File(Environment.getExternalStorageDirectory().getPath() + logFileName);
-        if (logFile.exists()) {
-            logFile.delete();
-        }
-    }
-
     private void addCallback(Object[] parametersAndHook) {
         parametersAndHook[parametersAndHook.length - 1] = new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 Log.d("LSPosedDebug", "Invoked: " + param.method);
-//                writeLogToFile("Invoked: " + param.method);
-                Log.d("LSPosedDebug", "Args: " + Arrays.toString(param.args));
-//                writeLogToFile("Args: " + Arrays.toString(param.args));
+                Log.d("LSPosedDebug", "Args: " + Arrays.deepToString(param.args));
             }
 
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Log.d("LSPosedDebug", "Result: " + param.getResult());
-//                writeLogToFile("Result: " + param.getResult());
             }
         };
     }
@@ -120,7 +96,6 @@ public class XPosedModule implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        clearLogFile();
         Log.d("LSPosedDebug", "Hooking into " + lpparam.packageName);
 
         try {
